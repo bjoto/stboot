6f5ff2d78f3be38370f696798093f0bd33148d33
Jens Drenhaus <jens.drenhaus@9elements.com>
2021-09-28 12:05:23 +0200

    write first test
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- config/host_config_test.go
+++ config/host_config_test.go
@@ -1,1 +1,15 @@
 package config
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestLoadHostCfg(t *testing.T) {
+	want := HostCfg{}
+	got := LoadHostCfg()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %V, want %V", got, want)
+	}
+}
