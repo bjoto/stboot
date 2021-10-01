0d28f3ac1b219c5086a767a2b03caa9ba63b8092
Jens Drenhaus <jens.drenhaus@9elements.com>
2021-10-01 15:36:35 +0200

    add missing test for custom error type
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

---
+++ config/config_test.go
@@ -0,0 +1,13 @@
+package config
+
+import "testing"
+
+func TestInvalidError(t *testing.T) {
+	msg := "some error message"
+	err := InvalidError(msg)
+
+	got := err.Error()
+	if got != msg {
+		t.Errorf("got %q, want %q", got, msg)
+	}
+}
